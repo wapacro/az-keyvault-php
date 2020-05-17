4e7bd3e3699d6d59e6c9e91dcbf50e01aff496b6
Roman Ackermann <roman@waprogramms.com>
2020-05-18 00:03:17 +0200

    Added key utility functions

---
+++ src/KeyUtil.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace AzKeyVault;
+
+
+use AzKeyVault\Responses\Key\KeyEntity;
+use Jose\Component\Core\JWK;
+use Jose\Component\Core\Util\ECKey;
+use Jose\Component\Core\Util\RSAKey;
+
+class KeyUtil {
+
+	/** @var KeyEntity */
+	protected $key;
+
+	/** @var JWK */
+	protected $jwk;
+
+	/**
+	 * KeyUtil constructor
+	 * @param KeyEntity $key
+	 */
+	public function __construct(KeyEntity $key) {
+		$this->key = $key;
+		$this->jwk = new JWK(array_filter([
+			'kty' => $key->type,
+			'crv' => $key->crv,
+			'x' => $key->x,
+			'y' => $key->y,
+			'd' => $key->d,
+			'dp' => $key->dp,
+			'dq' => $key->dq,
+			'e' => $key->e,
+			'n' => $key->n,
+			'p' => $key->p,
+			'q' => $key->q,
+			'qi' => $key->qi,
+			'k' => $key->k,
+		]));
+	}
+
+	/**
+	 * Returns the PEM represantation
+	 * of the key
+	 * @return string
+	 */
+	public function toPEM() {
+		if (!empty($this->key->crv)) {
+			// EC Key
+			return ECKey::convertToPEM($this->jwk);
+		}
+
+		// RSA Key
+		return RSAKey::createFromJWK($this->jwk)->toPEM();
+	}
+
+	/**
+	 * Returns the counterpart (public)
+	 * util instance for current (private) key
+	 * @return KeyUtil
+	 */
+	public function toPublicKey() {
+		$key = $this->key;
+		$privateParts = ['p', 'd', 'q', 'dp', 'dq', 'qi'];
+
+		foreach ($privateParts as $privatePart) {
+			$key->{$privatePart} = null;
+		}
+
+		return new self($key);
+	}
+
+}
