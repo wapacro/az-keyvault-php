8dec868b5ed8e6dcf9b9fb688588f2adb6f9f173
Roman Ackermann <roman@waprogramms.com>
2020-11-24 00:21:52 +0100

    Improved environment variable handling

--- src/Client.php
+++ src/Client.php
@@ -45,12 +45,24 @@ class Client {
 	 */
 	protected function getAccessToken() {
 		// Get MSI endpoint & token from environment (App Service) or use hardcoded values in case of VM
-		$endpoint = $_ENV['IDENTITY_ENDPOINT'] ?? 'http://169.254.169.254/metadata/identity/oauth2/token';
-		$idHeaderValue = $_ENV['IDENTITY_HEADER'] ?? 'true';
-		$idHeaderName = isset($_ENV['IDENTITY_ENDPOINT']) ? 'X-IDENTITY-HEADER' : 'Metadata';
+		$endpoint = $this->env('IDENTITY_ENDPOINT', 'http://169.254.169.254/metadata/identity/oauth2/token');
+		$idHeaderValue = $this->env('IDENTITY_HEADER', 'true');
+		$idHeaderName = !empty($this->env('IDENTITY_HEADER')) ? 'X-IDENTITY-HEADER' : 'Metadata';
 		$resource = 'https://vault.azure.net';
 
 		$endpoint = Url::fromString($endpoint)->withQueryParameter('resource', $resource);
 		return 'Bearer ' . $this->get($endpoint, $idHeaderValue, $idHeaderName, self::OAUTH_API_VERSION)->access_token;
+	}
+
+	/**
+	 * Returns the environment variable value if it
+	 * exists, otherwise the passed fallback value
+	 * @param string $name
+	 * @param string $fallback
+	 * @return array|string
+	 */
+	private function env(string $name, string $fallback = '') {
+		$value = getenv($name);
+		return $value !== false ? $value : $fallback;
 	}
 }
